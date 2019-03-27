77304b2c152b5d6d1c9149ec46263a0dff579adc
interpreted-character-4190 <amyequinn@hotmail.co.uk>
2019-03-27 18:23:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function happyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
-  var holiday ="tenerife";
+  var holiday ="Happy Independence Day";
   return (`Happy ${holiday}, ${name}!`);
 }
 
